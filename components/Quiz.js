ff52ffd67f623638b0b110256b731595db9ef07b
NelsonWang <wangnelson2@gmail.com>
2024-08-29 16:20:29 -0300

    roulette eli

---
+++ components/Quiz.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+
+const Quiz = ({ QuestionSet, defaultIndex = -1, selectCategory }) => {
+    // const [defaultIndex, setIndex] = useState(defaultIndex);
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [answerSubmitted, setShowResult] = useState(false);
+    const [is_answer_correct, setAnswerCorrect] = useState(false);
+
+    // useEffect(() => {
+
+        if (defaultIndex === -1 && QuestionSet?.length > 0) {
+            const randomIndex = Math.floor(Math.random() * QuestionSet.length);
+            setIndex(randomIndex);
+        }
+    //     console.log('Question set default index', defaultIndex, 'acula modified index', defaultIndex);
+    // }, [defaultIndex, QuestionSet]);
+
+    const handleOptionClick = (option) => {
+        if (answerSubmitted) {
+            return;
+        }
+        setSelectedOption(option);
+        handleSubmit();
+        if (option === QuestionSet[defaultIndex].answer) {
+            setAnswerCorrect(true);
+        } else {
+            setAnswerCorrect(false);
+        }
+    };
+
+    const handleSubmit = () => {
+        setShowResult(true);
+
+    };
+
+    const selectCategoryOption = (category) => {
+        
+        setSelectedOption(null);
+        setShowResult(false);
+        setAnswerCorrect(false);
+
+        selectCategory(category);
+    }
+
+    if (defaultIndex === -1 || !QuestionSet?.[defaultIndex]) {
+        return <div className="loading">Loading...</div>; // Handle case where question set is not ready
+    }
+
+    return (
+        <div className="quiz-container">
+            {/* <div className="quiz-container"></div> */}
+            <h2 className="question-text">{QuestionSet[defaultIndex].question}</h2>
+            <div className="options-container">
+                {QuestionSet[defaultIndex].options.map((option) => (
+
+                    <button
+                        onClick={() => handleOptionClick(option.label)}
+                        className={`option-button option ${answerSubmitted && option.label == QuestionSet[defaultIndex].answer ? 'correct_option' : ''} ${selectedOption === option.label ? (option.label != QuestionSet[defaultIndex].answer ? 'incorrect_selected' : '') : ''}`}
+                        key={option.label}
+                    >
+                        {option.value}
+                    </button>
+                ))}
+            </div>
+
+
+            {answerSubmitted && (
+                <div>
+                    <br />
+
+                    <button className='redirect-button option' onClick={() => selectCategoryOption('nails')}>Nails</button>
+                    <button className='redirect-button option' onClick={() => selectCategoryOption('beauty')} >Beauty</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Quiz;
---
+++ pages/index.js
@@ -0,0 +1,197 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Quiz from '../components/Quiz';
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [nailsIdx, setNailsIdx] = useState(0);
+  const [beautyIdx, setBeautyIdx] = useState(0);
+  const question_sample_nails = [
+    {
+      "question": "¿Qué es la manicuría?",
+      "options": [
+        { "label": "A", "value": "Cuidado y arreglo de las uñas de las manos" },
+        { "label": "B", "value": "Corte de cabello" },
+        { "label": "C", "value": "Exfoliación de la piel" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Cuál es la función principal de una lima de uñas?",
+      "options": [
+        { "label": "A", "value": "Cortar las uñas" },
+        { "label": "B", "value": "Dar forma y suavizar los bordes" },
+        { "label": "C", "value": "Pintar las uñas" }
+      ],
+      "answer": "B"
+    },
+    {
+      "question": "¿Qué herramienta se utiliza para cortar las cutículas?",
+      "options": [
+        { "label": "A", "value": "Alicate de cutículas" },
+        { "label": "B", "value": "Lima de uñas" },
+        { "label": "C", "value": "Pinzas" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Cuánto tiempo se recomienda dejar las uñas en remojo antes de un servicio de manicuría?",
+      "options": [
+        { "label": "A", "value": "1 minuto" },
+        { "label": "B", "value": "5-10 minutos" },
+        { "label": "C", "value": "30 minutos" }
+      ],
+      "answer": "B"
+    },
+    {
+      "question": "¿Qué es un esmalte base y para qué sirve?",
+      "options": [
+        { "label": "A", "value": "Un esmalte transparente para proteger las uñas" },
+        { "label": "B", "value": "Un esmalte para dar color" },
+        { "label": "C", "value": "Un esmalte para brillo" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Cuántas capas de esmalte de color se deben aplicar en una manicura tradicional?",
+      "options": [
+        { "label": "A", "value": "Una" },
+        { "label": "B", "value": "Dos" },
+        { "label": "C", "value": "Tres" }
+      ],
+      "answer": "B"
+    },
+    {
+      "question": "¿Qué es un top coat y para qué se utiliza?",
+      "options": [
+        { "label": "A", "value": "Un esmalte transparente para sellar y dar brillo" },
+        { "label": "B", "value": "Un producto para suavizar cutículas" },
+        { "label": "C", "value": "Un removedor de esmalte" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Cuál es la función de la cutícula?",
+      "options": [
+        { "label": "A", "value": "Proteger la base de la uña" },
+        { "label": "B", "value": "Aumentar el crecimiento de la uña" },
+        { "label": "C", "value": "Dar color a la uña" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Por qué es importante desinfectar las herramientas de manicuría?",
+      "options": [
+        { "label": "A", "value": "Para mantenerlas en buen estado" },
+        { "label": "B", "value": "Para prevenir infecciones" },
+        { "label": "C", "value": "Para que las uñas se vean mejor" }
+      ],
+      "answer": "B"
+    },
+    {
+      "question": "¿Qué tipo de lima es mejor para uñas naturales: metálica o de cartón?",
+      "options": [
+        { "label": "A", "value": "Metálica" },
+        { "label": "B", "value": "De cartón" },
+        { "label": "C", "value": "De vidrio" }
+      ],
+      "answer": "B"
+    }
+  ]
+
+  const question_sample_beauty = [
+    {
+      "question": "¿Qué es un facial?",
+      "options": [
+        { "label": "A", "value": "Un tratamiento para la piel del rostro" },
+        { "label": "B", "value": "Un masaje" },
+        { "label": "C", "value": "Un tratamiento para el cabello" }
+      ],
+      "answer": "A"
+    },
+    {
+      "question": "¿Qué es un exfoliante?",
+      "options": [
+        { "label": "A", "value": "Un producto para limpiar el rostro" },
+        { "label": "B", "value": "Un producto para eliminar células muertas" },
+        { "label": "C", "value": "Un producto para hidratar la piel" }
+      ],
+      "answer": "B"
+    }
+  ]
+
+  useEffect(() => {
+    // randomize questions order.
+    question_sample_nails.sort(() => Math.random() - 0.5);
+    question_sample_beauty.sort(() => Math.random() - 0.5);
+  }, []);
+
+  const selectCategoryClick = (category, increase = true, is_correct = true) => {
+    let nailsIdxNew = nailsIdx;
+    let beautyIdxNew = beautyIdx;
+    if (increase) {
+      if (category == 'nails') {
+        nailsIdxNew = nailsIdx + 1;
+        if (nailsIdxNew > question_sample_nails.length - 1) {
+          nailsIdxNew = 0;
+        }
+        
+        
+      } else if (category == 'beauty') {
+        beautyIdxNew = beautyIdx + 1;
+        if (beautyIdxNew > question_sample_beauty.length - 1) {
+          beautyIdxNew = 0;
+        }
+      }
+
+      setBeautyIdx(beautyIdxNew);
+      setNailsIdx(nailsIdxNew);
+    }
+    
+
+    setSelectedCategory(category);
+
+
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meli Quiz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          {
+            selectedCategory == 'nails' && (
+              <Quiz QuestionSet={question_sample_nails} selectCategory={selectCategoryClick} defaultIndex={nailsIdx}></Quiz>
+            )
+          }
+          {
+            selectedCategory == 'beauty' && (
+              <Quiz QuestionSet={question_sample_beauty} selectCategory={selectCategoryClick} defaultIndex={beautyIdx}></Quiz>
+            )
+          }
+
+          {selectedCategory == null && (
+            <div>
+              <div className="quiz-container">
+                <br />
+                <button className='redirect-button option' onClick={() => selectCategoryClick('nails', false)} >Nails</button>
+                <button className='redirect-button option' onClick={() => selectCategoryClick('beauty', false)}  >Beauty</button>
+
+              </div>
+            </div>
+          )}
+        </div>
+
+
+      </main>
+
+    </div>
+  );
+}
